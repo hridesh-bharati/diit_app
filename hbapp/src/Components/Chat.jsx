7bfcdbfa4ffe110fc06f6ba7bb16363dba681ad5
Hridesh Bharati <116611293+hridesh-bharati@users.noreply.github.com>
2023-12-11 19:20:15 +0530

    Verification.jsx
    Hb

--- hbapp/src/Components/Chat.jsx
+++ hbapp/src/Components/Chat.jsx
@@ -1,94 +1,86 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 
-// const YourComponent = () => {
-//     const [checkboxChecked, setCheckboxChecked] = useState(false);
-//     const [reCaptchaCompleted, setReCaptchaCompleted] = useState(false);
-//     const [submit, setSubmit] = useState(false);
-//     const [message, setMessage] = useState('');
+const VerificationForm = () => {
+    const [imgPreview, setImgPreview] = useState("");
+    const [error, setError] = useState("");
 
-//     useEffect(() => {
-//         const script = document.createElement('script');
-//         script.src = 'https://www.google.com/recaptcha/api.js';
-//         script.async = true;
-//         script.defer = true;
-//         document.head.appendChild(script);
+    const handleFileChange = (event) => {
+        const files = event.target.files;
 
-//         script.onload = () => {
-//             window.grecaptcha.render('g-recaptcha', {
-//                 sitekey: '6LeSkxwpAAAAAJr0__9WFMn2k3bJ9EW1eT52aaqm',
-//                 callback: () => {
-//                     console.log('reCAPTCHA completed!');
-//                     setReCaptchaCompleted(true);
-//                 },
-//             });
-//         };
+        if (files.length > 0) {
+            const selectedFile = files[0];
 
-//         return () => {
-//             document.head.removeChild(script);
-//         };
-//     }, []);
+            // Check if the file size is less than or equal to 50 KB
+            if (selectedFile.size <= 50 * 1024) {
+                const fileReader = new FileReader();
 
-//     const postForm = () => {
-//         // Add your logic to submit the form data
-//         // For example, you can make an API call here
+                fileReader.onload = () => {
+                    setImgPreview(fileReader.result);
+                    setError("");
+                };
 
-//         // Update state variables accordingly
-//         setSubmit(true);
-//         setMessage('Submitted Successfully');
-//     };
+                fileReader.readAsDataURL(selectedFile);
+            } else {
+                setImgPreview("");
+                setError("Upload only 50 KB smaller image.");
+            }
+        }
+    };
 
-//     return (
-//         <div className="col-12 px-1" style={{ marginTop: '10rem' }}>
-//             <div className="form-check">
-//                 <div
+    return (
+        <div style={{ marginTop: '4rem' }}>
+            <form className=" g-4 m-0 px-md-3  m-auto text-center myshadow border border-light bg-white " id="verifyParentBg">
+                <div className="col-12 m-0 p-0 " style={{ border: '1px solid #012C5', background: 'var(--card-bg)' }}>
+                    <div className="container text-center m-0 py-3 h2 fw-bold text-uppercase " style={{ color: 'rgb(255, 255, 255)' }}>
+                        <b style={{ letterSpacing: '1px', color: 'white' }}>
+                            Upload  <font color="red">Certificate Data</font>
+                        </b>
+                    </div>
+                </div>
+                <div class="row">
 
-//                 >
+                    <div class="col-md-6">
+                        <input type="text" class="form-control my-2" placeholder="Student Name" />
+                        <input type="text" class="form-control my-2" placeholder="Complete Duration in Months" />
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control my-2" placeholder="Course Name (Full)" />
+                        <input type="text" class="form-control my-2" placeholder="Grade & Percentage " />
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control my-2" placeholder="Course Name (Full)" />
+
+                        <div className="input-group mb-3 p-0 m-0 d-flex justify-content-center align-items-center" > <i className="bi bi-calendar-month  input-group-text "></i>
+                            <input type="date" class="form-control my-2" placeholder="Date of issue" />
+
+                        </div>
+
+                        <input type="file" accept="image/*" className="form-control my-2 float-start" id="choose-file" onChange={handleFileChange} />
+                    </div>
 
 
-//                 </div>
-//                 <div className="bg-success-subtle text-danger p-2 rounded rounded-pill d-inline">
-//                     <label className="form-check-label h5" htmlFor="invalidCheck">
-//                         <input
-//                             type="checkbox"
-//                             className="validCheck"
-//                             required
-//                             onChange={(e) => setCheckboxChecked(e.target.checked)}
-//                         />
-//                         <span className="icon"> </span>
-//                         <span style={{ fontSize: '1.2rem !important' }}>Declaration</span>
-//                         <span className="text-body-secondary"> / घोषणा </span>
-//                     </label>
-//                 </div>
-//             </div>
-//             <div className="col-12 ms-2" >
-//                 <div className="g-recaptcha" id="g-recaptcha"
-//                     data-sitekey="6LeSkxwpAAAAAJr0__9WFMn2k3bJ9EW1eT52aaqm"
-//                     onChange={(ex) => setReCaptchaCompleted(ex.target.checked)}></div>
+                    <div class="col-md-6">
+                        {imgPreview && (
+                            <div id="img-preview" style={{ display: "block" }}>
+                                <img src={imgPreview} style={{ maxWidth: '100px' }} className="border" alt="Preview" />
+                            </div>
+                        )}
+                    </div>
 
-//                 {submit ? (
-//                     <button className="btn btn-success ms-3" id="checkBtn" type="button">
-//                         Submitted Successfully
-//                     </button>
-//                 ) : (
-//                     <button
-//                         className="btn btn-primary ms-3"
-//                         id="checkBtn"
-//                         type="button"
-//                         onClick={() => {
-//                             if (checkboxChecked  reCaptchaCompleted) {
-//                                 postForm();
-//                             }
-//                         }}
-//                         disabled={!checkboxChecked && !reCaptchaCompleted}
-//                     >
-//                         Submit
-//                     </button>
-//                 )}
-//                 <div className="text-danger d-inline px-1">{message}</div>
-//             </div>
-//         </div>
-//     );
-// };
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        {error && <div style={{ color: "red", fontSize: '0.7rem' }}>{error}</div>}
+                    </div>
 
-// export default YourComponent;
+                </div>
+                <div className="col-md-12 pb-2 text-center mt-5">
+                    <button type="button" className="btn btn-sm btn-primary text-white 
+                                yellow-btn border border-white fw-bold" > Upload </button>
+                </div>
+            </form>
 
+
+        </div>
+    );
+};
+
+export default VerificationForm;
